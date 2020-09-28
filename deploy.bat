91f7261efdf99671f98e9e9741bd2407a74f3d7c
unknown <minecraft.kyle.train@gmail.com>
2020-09-29 00:45:16 +0800

    9/29 gitignore update & deploy.bat cut off git

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,7 @@
 @ECHO OFF
-set commitMessage=%1
 echo STARTING DEPLOYING TO FIREBASE (Vue.js)...
 cd %~dp0\client\
 cmd /c npm run build
 cmd /c firebase deploy --project 726820887406
 cd ..
-echo STARTING DEPLOYING TO HEROKU (Node.js)...
-cd %~dp0\server\
-cmd /WAIT git add .
-cmd /WAIT git commit -m "%commitMessage%"
-cmd /c git push heroku master
-cd ..
-cmd /WAIT git push api "%commitMessage%"
 echo ALL PROCESS ENDED, EXIT...
